61b26ee173f10414025d5950009eaab9f36b4ce2
hramirez289 <hernan.stgohc@gmail.com>
2024-04-08 22:58:59 -0400

    primera commit

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import index
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', index, name='home'),
+    
+]
