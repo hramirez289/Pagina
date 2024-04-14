5da22fb095a63f624efe4653eabff19440b7df06
hramirez289 <hernan.stgohc@gmail.com>
2024-04-13 21:14:28 -0400

    commit de todos los archivos modificados

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import index
+from . import views
+# from .views import index, accion, deportes, disparos, estrategia, formulario, rol
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', views.index, name='index'),
+    path('accion', views.accion, name='accion'),
+    path('deportes', views.deportes, name='deportes'),
+    path('disparos', views.disparos, name='disparos'),
+    path('estrategia', views.estrategia, name='estrategia'),
+    path('rol', views.Rol, name='rol'),
+    path('formulario', views.formulario, name='formulario'),
     
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,3 +4,21 @@ from django.conf.urls.static import static
 
 def index(request):
     return render(request, 'index.html')
+
+def accion(request):
+    return render(request, 'accion.html')
+
+def deportes(request):
+    return render(request, 'deportes.html')
+
+def disparos(request):
+    return render(request, 'disparos.html')
+
+def estrategia(request):
+    return render(request, 'estrategia.html')
+
+def Rol(request):
+    return render(request, 'Rol.html')
+
+def formulario(request):
+    return render(request, 'formulario.html')
