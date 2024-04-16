2a71b9cdfd49693f4a395a58a15edb4bb9c13ad5
hramirez289 <hernan.stgohc@gmail.com>
2024-04-15 22:49:41 -0400

    modificaciones bd

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre_completo = models.CharField(max_length=100)
+    nombre_usuario = models.CharField(max_length=50)
+    correo_electronico = models.EmailField()
+    clave_acceso = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    direccion_despacho = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre_usuario
